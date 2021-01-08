2ea4056ca8a3bc2c03e7dd63e6a224a0ea6f121b
luiz <luiz.fhs2015@gmail.com>
2021-01-08 12:38:12 -0300

    aula11

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+//console.log("ae!!!!!!!");/*document.wirte não ira funcionar na extenção .js*/
+var vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if(vel > 60){//condição simples
+    console.log('Você ultrapassou a velocidade permitidade. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!') 
---
+++ Aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+console.log(`Vinvendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro!')
+}
+else{
+    console.log('Você é Estrangeiro!')
+}
